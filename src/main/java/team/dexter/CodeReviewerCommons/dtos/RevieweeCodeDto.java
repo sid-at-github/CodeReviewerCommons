d3557c8c2245b1e3dc6989123f5e8586a2328bf9
Siddharth Papreja <sidd.papreja@gmail.com>
2019-11-17 14:57:12 +0000

    added common code

--- src/main/java/team/dexter/CodeReviewerCommons/dtos/RevieweeCodeDto.java
+++ src/main/java/team/dexter/CodeReviewerCommons/dtos/RevieweeCodeDto.java
@@ -4,10 +4,19 @@ import java.util.List;
 
 public class RevieweeCodeDto {
 
+	private String id;
 	private String username;
 	private String title;
 	private String code;
 	private List<String> tags;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getUsername() {
 		return username;
